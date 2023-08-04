b259f271d314917b38ad7e108a0d7b6e998a85a1
Rohid <rohidul209@gmail.com>
2023-08-04 17:35:13 +0600

    tinybird analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        {process.env.NODE_ENV === "production" && (
+          <script
+            defer
+            src="https://unpkg.com/@tinybirdco/flock.js"
+            data-host="https://api.us-east.tinybird.co"
+            data-token="p.eyJ1IjogIjFjMzk3M2IzLTMwMTQtNDAyOS04MzQ5LThiMDY0ZDY0NDljMiIsICJpZCI6ICIyZmE4NTFhYS1mNzhmLTQ3ZjEtYTQ1YS02ZDIxZWU3MGFiYWUiLCAiaG9zdCI6ICJ1c19lYXN0In0.vFFS3LTMAqCSHDRaOwCpZTCDdR_BGQNahOrgCA87XR8"
+          />
+        )}
+      </head>
       <body>{children}</body>
     </html>
   );
